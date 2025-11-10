7175a82fbcafa9f08d0ccc63098f0ea3fafb333b
Phoebe Taylor <phoebe.t@wustl.edu>
2025-11-10 11:56:02 -0600

    making an array of 9 nulls corresponding with 9 squares so the board can hold all of the info for each square

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 
 function Square() {
@@ -19,22 +18,23 @@ function Square() {
 }
 
 export default function Board() {
+   const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
