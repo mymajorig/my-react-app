8589ac6bddd7243abe8034e4c22adbd3a1069afb
Phoebe Taylor <phoebe.t@wustl.edu>
2025-11-10 11:59:10 -0600

    removing other code from square component and replacing it with a line that will recieve the value prop from the board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
-   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
-       <div className="board-row">
+      <div className="board-row">
         <Square value={squares[0]} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
