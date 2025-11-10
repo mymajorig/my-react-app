7d84353f0d6a37cda6545cf9d7237cc881794159
Phoebe Taylor <phoebe.t@wustl.edu>
2025-11-10 11:43:22 -0600

    including 'states' so the square can remember that it's been clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+function Square() {
+
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
-    <div className="board-row">
+      <div className="board-row">
         <Square value="1" />
         <Square value="2" />
         <Square value="3" />
