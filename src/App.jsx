a2a3e2444ba993b6c9fb2c438a81b0910f1d2f64
Phoebe Taylor <phoebe.t@wustl.edu>
2025-11-10 13:56:35 -0600

    creating jumpTo function and maping the history array to display buttons to jump to past moves

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,23 @@ export default function Game() { //export default --> use game component as top 
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+   const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   
   return (
     <div className="game">
