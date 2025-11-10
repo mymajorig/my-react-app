c41094602aa81204cfdfadda7647800064d96e47
Phoebe Taylor <phoebe.t@wustl.edu>
2025-11-10 13:34:18 -0600

    creating game component that will store the games history state

--- src/App.jsx
+++ src/App.jsx
@@ -74,3 +74,16 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
